67486c0008f929dc3b0db37dc90935868bdef5a7
Craig Gardner <craig.s.gardner@gmail.com>
2020-10-24 08:29:09 -0700

    improvement(jira): added issue type as a parameter
    Also added an alias to edit the jira scripts quickly

--- .config/zsh/config/jira.sh
+++ .config/zsh/config/jira.sh
@@ -4,13 +4,15 @@
 # Functions {{{
 function jira_create() {
   local project="$1"
+  local issueType="${2:-Story}"
   local jira=$(which jira)
 
-  $jira create -p $project -i "Story" --override reporter=$(whoami)
+  $jira create -p $project -i "$2" --override reporter=$(whoami)
 }
 # }}}
 
 
 # Aliases {{{
 alias ,jc="jira_create"
+alias ,je="$EDITOR $0 && source $0"
 # }}}
