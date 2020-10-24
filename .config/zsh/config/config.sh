e6c88d71295996226a704a3788254ee016855e1f
Craig Gardner <craig.s.gardner@gmail.com>
2020-10-24 08:31:34 -0700

    improvement(config): changed the way i manage my config helper

--- .config/zsh/config/config.sh
+++ .config/zsh/config/config.sh
@@ -1,1 +1,15 @@
-alias config="$(which git) --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+# Utilities {{{
+function _config() {
+  local git=$(which git)
+  local flags=("--git-dir=${HOME}/.cfg" "--work-tree=${HOME}")
+  case $1 in
+    *)
+      $git $flags $@
+      ;;
+  esac
+}
+# }}}
+# Aliases {{{
+alias ,c="_config"
+alias ,ce="$EDITOR $0 && source $0"
+# }}}
