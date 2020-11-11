b2d9be85661bdac06e4c4d1955e75f866cfe27a2
Craig Gardner <craig.s.gardner@gmail.com>
2020-11-11 07:30:40 -0800

    chore(config): added a shortcut to reset all changes

--- .config/zsh/config/config.sh
+++ .config/zsh/config/config.sh
@@ -3,6 +3,9 @@ function _config() {
   local git=$(which git)
   local flags=("--git-dir=${HOME}/.cfg" "--work-tree=${HOME}")
   case $1 in
+    "reset")
+      $git $flags reset HEAD
+      ;;
     *)
       $git $flags $@
       ;;
