1117cf86a8f897951fa614e495bc776be0b91d8e
Craig Gardner <craig.s.gardner@gmail.com>
2021-12-30 12:51:10 -0800

    chore(nvim): added ledger formatter

--- .config/nvim/lua/plugins/neoformat.lua
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -1,7 +1,13 @@
 local M = {}
 
-function config()
+local function config()
     vim.api.nvim_set_keymap("n", "<C-f>", ":Neoformat<CR>", {})
+
+    vim.g.neoformat_ledger_ledger = {
+      exe = "ledger",
+      args = {"-f", "- print"}
+    }
+    vim.g.neoformat_enabled_ledger = {"ledger"}
 end
 M.config = config
 
