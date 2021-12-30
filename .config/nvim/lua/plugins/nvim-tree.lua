1afa065dacbec7b4d3c59f6da8e55ff54b34a130
Craig Gardner <craig.s.gardner@gmail.com>
2021-12-30 12:51:40 -0800

    feat(nvim): ignoring .git and .gitignore in nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,6 +4,10 @@ local function config()
     require'nvim-tree'.setup {
       disable_netrw = false,
       hijiack_netrw = false,
+      filters = {
+        dotfiles = true,
+        custom = {".git", ".gitignore"},
+      },
     }
     vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', {})
 end
