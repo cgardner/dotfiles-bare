9731352b8825b2e4fd4b3887df7bc4007ac9ffcb
Craig Gardner <craig.s.gardner@proton.me>
2023-11-19 09:29:10 -0800

    feat: switching tmuxinator config for nix setup

--- .config/nix/home/default.nix
+++ .config/nix/home/default.nix
@@ -44,7 +44,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   # home.file = import ./files.nix;
-  # home.file = {
+  home.file = {
+    ".config/tmuxinator".source = ./tmuxinator;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -55,7 +56,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  # };
+  };
 
   # You can also manage environment variables but you will have to manually
   # source
