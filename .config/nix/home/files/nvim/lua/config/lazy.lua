6703162866c2bc2662cc808e75b0bc3a040c173d
Craig Gardner <craig.s.gardner@proton.me>
2024-03-29 10:29:20 -0700

    feat(nvim): added aerial.nvim extra

--- .config/nix/home/files/nvim/lua/config/lazy.lua
+++ .config/nix/home/files/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.vscode" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
